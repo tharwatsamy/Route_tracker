05e800da6824522450ecf9c7c115a7d117921416
tharwatsamy <tharwatsamy2004@gmail.com>
2024-02-27 02:33:10 +0200

    refactor code part two

--- lib/utils/map_services.dart
+++ lib/utils/map_services.dart
@@ -1,4 +1,13 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:route_tracker/models/location_info/lat_lng.dart';
+import 'package:route_tracker/models/location_info/location.dart';
+import 'package:route_tracker/models/location_info/location_info.dart';
 import 'package:route_tracker/models/place_autocomplete_model/place_autocomplete_model.dart';
+import 'package:route_tracker/models/routes_model/routes_model.dart';
 import 'package:route_tracker/utils/google_maps_place_service.dart';
 import 'package:route_tracker/utils/location_service.dart';
 import 'package:route_tracker/utils/routes_service.dart';
@@ -21,5 +30,95 @@ class MapServices {
     } else {
       places.clear();
     }
+  }
+
+  Future<List<LatLng>> getRouteData(
+      {required LatLng currentLocation, required LatLng desintation}) async {
+    LocationInfoModel origin = LocationInfoModel(
+      location: LocationModel(
+          latLng: LatLngModel(
+        latitude: currentLocation.latitude,
+        longitude: currentLocation.longitude,
+      )),
+    );
+    LocationInfoModel destination = LocationInfoModel(
+      location: LocationModel(
+          latLng: LatLngModel(
+        latitude: desintation.latitude,
+        longitude: desintation.longitude,
+      )),
+    );
+    RoutesModel routes = await routesService.fetchRoutes(
+        origin: origin, destination: destination);
+    PolylinePoints polylinePoints = PolylinePoints();
+    List<LatLng> points = getDecodedRoute(polylinePoints, routes);
+    return points;
+  }
+
+  List<LatLng> getDecodedRoute(
+      PolylinePoints polylinePoints, RoutesModel routes) {
+    List<PointLatLng> result = polylinePoints.decodePolyline(
+      routes.routes!.first.polyline!.encodedPolyline!,
+    );
+
+    List<LatLng> points =
+        result.map((e) => LatLng(e.latitude, e.longitude)).toList();
+    return points;
+  }
+
+  void displayRoute(List<LatLng> points,
+      {required Set<Polyline> polyLines,
+      required GoogleMapController googleMapController}) {
+    Polyline route = Polyline(
+      color: Colors.blue,
+      width: 5,
+      polylineId: const PolylineId('route'),
+      points: points,
+    );
+
+    polyLines.add(route);
+
+    LatLngBounds bounds = getLatLngBounds(points);
+    googleMapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 32));
+  }
+
+  LatLngBounds getLatLngBounds(List<LatLng> points) {
+    var southWestLatitude = points.first.latitude;
+    var southWestLongitude = points.first.longitude;
+    var northEastLatitude = points.first.latitude;
+    var northEastLongitude = points.first.longitude;
+
+    for (var point in points) {
+      southWestLatitude = min(southWestLatitude, point.latitude);
+      southWestLongitude = min(southWestLongitude, point.longitude);
+      northEastLatitude = max(northEastLatitude, point.latitude);
+      northEastLongitude = max(northEastLongitude, point.longitude);
+    }
+
+    return LatLngBounds(
+        southwest: LatLng(southWestLatitude, southWestLongitude),
+        northeast: LatLng(northEastLatitude, northEastLongitude));
+  }
+
+  Future<LatLng> updateCurrentLocation(
+      {required GoogleMapController googleMapController,
+      required Set<Marker> markers}) async {
+    var locationData = await locationService.getLocation();
+
+    var currentLocation =
+        LatLng(locationData.latitude!, locationData.longitude!);
+
+    Marker currentLocationMarker = Marker(
+      markerId: const MarkerId('my location'),
+      position: currentLocation,
+    );
+    CameraPosition myCurrentCameraPoistion = CameraPosition(
+      target: currentLocation,
+      zoom: 17,
+    );
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(myCurrentCameraPoistion));
+    markers.add(currentLocationMarker);
+    return currentLocation;
   }
 }
